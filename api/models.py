7d3b78ef46092b8624c348b5e0c4e7576cb5431b
HARD953 <issa97403@gmail.com>
2025-08-16 14:52:29 +0000

    master16

--- api/models.py
+++ api/models.py
@@ -502,7 +502,6 @@ class Notification(models.Model):
         verbose_name_plural = "Notifications"
 
 
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
